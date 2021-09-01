10941ba07c00be10de16d4e51a49fc4636a677ad
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-01 08:20:41 +0400

    Add old style notification handling example

--- publishers-and-subscribers.playground/Contents.swift
+++ publishers-and-subscribers.playground/Contents.swift
@@ -1,1 +1,20 @@
+import Foundation
 import Combine
+
+example(of: "Old Style") {
+    let notif = Notification.Name("MyNotification")
+    let center = NotificationCenter.default
+        
+    let observer = center.addObserver(
+        forName: notif,
+        object: nil,
+        queue: nil,
+        using: { notification in
+            print("Notification Received!")
+        }
+    )
+    
+    center.post(name: notif, object: nil)
+    
+    center.removeObserver(observer)
+}
